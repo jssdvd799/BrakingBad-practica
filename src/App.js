442ee933c7ed5f016d9180e1877f771f48b79bf0
jssdvd799 <perso-29@protonmail.com>
2020-01-20 16:00:18 -0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import Frase from './componentes/Frase';
 
@@ -7,42 +7,53 @@ const Contenedor = styled.div`
   align-items: center;
   padding-top: 5rem;
   flex-direction: column;
-`;
+` ;
 
 const Boton = styled.button`
-  background: -webkit-linear-gradient(
-    top left,
-    #007d35 0%,
-    #007d35 40%,
-    #0f574e 100%
-  );
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
   background-size: 300px;
-  fontfamily: Helvetica, Arial, sans-serif;
+  font-family:  Arial, Helvetica, sans-serif;
   color: #fff;
   margin-top: 3rem;
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor:pointer;
+    background-size: 400px;
+  }
 `;
 
 function App() {
 
-  // State de frases
-  const { frase, guardarFrase } = useState({});
+  // state de frases
+  const [frase, guardarFrase] = useState({});
+
 
   const consultarAPI = async () => {
-    const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
     const frase = await api.json()
     guardarFrase(frase[0]);
-  };
+  }
+
+  // Cargar una frase
+  useEffect(() => {
+    consultarAPI()
+  }, []);
 
   return (
-
     <Contenedor>
       <Frase
         frase={frase}
       />
-      <Boton onClick={consultarAPI}>Obtener frase nueva</Boton>
+
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener Frase
+      </Boton>
     </Contenedor>
   );
 }
